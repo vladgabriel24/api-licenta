de199227d3d2b3b94010d1d2b4e4911637aba821
vladgabriel24 <vlad.dumitru2402@gmail.com>
2024-06-04 20:15:46 +0300

    refactor

--- update_db.sh
+++ update_db.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 cron_schedule="* * * * *"
-curl_command="curl -X POST http://$2:8080/load-db"
+curl_command="curl -X POST http://$2:8080/api/load-db"
 
 # Verificam exista lockfile-ul
 lock=$(ls -al / | grep monitoring_lockfile)
